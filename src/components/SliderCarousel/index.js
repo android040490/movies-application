78a574c782364a2106ff122e7c3d739099c70bcd
Pypkin <apalkov040490@gmail.com>
2018-09-14 16:55:28 +0300

    changed styles SliderCarousel and FilmInfo components, added react-responsive package

--- src/components/SliderCarousel/index.js
+++ src/components/SliderCarousel/index.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import Slider from "react-slick";
 import {Link, withRouter} from  "react-router";
+import MediaQuery from 'react-responsive';
 
 class SliderCarousel extends Component {
+
+    renderSlider(settings, category){
+      return(
+        <Slider {...settings}>
+            {this.props.movies.map( item => {
+                return(
+                  <Link to={`/movie?category=${category}&movieId=${item.id}`} className="carousel__item" key={item.id}>
+                    <div className="carousel__item-img"><img src={`https://image.tmdb.org/t/p/w250_and_h141_face${item.backdrop_path}`} alt=""/></div>
+                    <h3 className="carousel__item-title">{item.title}</h3>
+                  </Link>
+                ) 
+              })
+            }
+        </Slider>
+      )
+    }
+
     render() {
       let { category } = this.props.location.query
       const settings = {
@@ -16,17 +34,15 @@ class SliderCarousel extends Component {
       };
       return (
         <div className="carousel">
-          <Slider {...settings}>
-            {this.props.movies.map( item => {
-                return(
-                  <Link to={`/movie?category=${category}&movieId=${item.id}`} className="carousel__item" key={item.id}>
-                    <div className="carousel__item-img"><img src={`https://image.tmdb.org/t/p/w250_and_h141_face${item.backdrop_path}`} alt=""/></div>
-                    <h3 className="carousel__item-title">{item.title}</h3>
-                  </Link>
-                ) 
-              })
-            }
-          </Slider>
+          <MediaQuery maxWidth={599}>
+             {this.renderSlider({...settings, slidesToShow : 1, slidesToScroll : 1 , dots: false}, category)}
+          </MediaQuery>
+          <MediaQuery minWidth={600} maxWidth={999}>
+             {this.renderSlider({...settings, slidesToShow : 2, slidesToScroll : 2, dots: false}, category)}
+          </MediaQuery>
+          <MediaQuery minWidth={1000}>
+             {this.renderSlider({...settings}, category)}
+          </MediaQuery>
         </div>
       );
     }
