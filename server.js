5c9f96f94681ff3f5e0565265acc913c774caa07
Pypkin <apalkov040490@gmail.com>
2018-10-10 16:10:20 +0300

    changed file server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 let app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/'));
+app.use('/dist', express.static('dist'));
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'), (err) => {
+        if (err){
+            res.status(500).send(err)
+        }
+    })
+})
+
 
 app.listen(PORT, function(){
     console.log('Express server is up on port' + PORT);
